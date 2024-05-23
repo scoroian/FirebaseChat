029ec7b71fc46690ecaa6979c2301f2de9e75fad
scoroian <sebastiancoroian@gmail.com>
2024-05-23 20:41:51 +0200

    splash activity gif

--- app/src/main/java/com/scoroian/firebasechat/SplashActivity.kt
+++ app/src/main/java/com/scoroian/firebasechat/SplashActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.scoroian.firebasechat.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
@@ -15,6 +17,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.cat_spinning)
+            .into(imageView)
 
     }
 }
